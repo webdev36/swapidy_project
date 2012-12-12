ea4116e15ff8a1f143f3e2a08f118ed87ccd49ae
Pulkit Jaiswal <pulkitj@stanford.edu>
2012-12-12 12:24:24 -0800

    added backend

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Swapidy::Application.config.secret_token = '143131cea159ee744a499563fd0c202e854271f5d14d4fc61bf57ab4660e5a3eee4c53421f86e9cf80553fb39bd5d56b46a7719bf2a92d96720288668cdc0e54'
