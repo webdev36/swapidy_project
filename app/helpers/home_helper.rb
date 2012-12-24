fbdaafb1ef5ec3ee8f7eb35defdd9b63cd681f8b
Bui Thanh Hai <hai.bth@gmail.com>
2012-12-24 10:00:53 +0700

    Make big checkbox in filters

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -4,7 +4,7 @@ module HomeHelper
     html = %Q{<h3 class="s_category">Using conditions</h3>}
     Product::USING_CONDITIONS.keys.each do |condition|
       html += %Q{
-                  <input type="checkbox" attr-filter-option='#{condition.to_s}'>#{Product::USING_CONDITIONS[condition]}</input>
+                  <input type="checkbox" class="regular_checkbox" attr-filter-option='#{condition.to_s}'>#{Product::USING_CONDITIONS[condition]}</input>
                 }
     end
     raw html
