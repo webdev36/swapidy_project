32303e334430a7e5e1bdc1365202dfb254919ee6
Pulkit Jaiswal <pulkitj@stanford.edu>
2013-01-02 22:22:54 -0800

    design changes

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,7 +1,7 @@
 module HomeHelper
   
   def filter_using_conditions
-    html = %Q{<h3 class="s_category">Using conditions</h3>}
+    html = %Q{<h3 class="s_category">Conditions</h3>}
     Product::USING_CONDITIONS.keys.each do |condition|
       html += %Q{
                   <input type="checkbox" class="regular_checkbox" attr-filter-option='#{condition.to_s}'>#{Product::USING_CONDITIONS[condition]}</input>
