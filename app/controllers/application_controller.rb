2530ff2937cc48bebb54a33a0e948bdab80f8111
Bui Thanh Hai <hai.bth@gmail.com>
2013-01-06 00:20:06 +0700

    Add check_uri to redirect to www domain

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
+  before_filter :check_uri
+
+  def check_uri
+    if Rails.env.production?
+      redirect_to request.protocol + "www." + request.host_with_port + request.request_uri if !/^www/.match(request.host)
+    end
+  end
+  
   def require_login
     unless user_signed_in?
       redirect_to new_user_session_path, :flash => { :info => "You must first log in or sign up before accessing this page." }
