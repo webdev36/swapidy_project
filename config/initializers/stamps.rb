6f3b6f9be9287f711a194ea414733824f6c03220
Bui Thanh Hai <hai.bth@gmail.com>
2013-01-23 15:32:17 -0800

    Commit codes for use Stamp Env variables

--- config/initializers/stamps.rb
+++ config/initializers/stamps.rb
@@ -10,11 +10,19 @@ Stamps.configure do |config|
   # If your default_local is different from :en, uncomment the following 2 lines and set your default locale here:
   # require 'i18n'
   # I18n.default_locale = :de
-  config.integration_id = "698b2f76-8f72-4359-b04f-1ca8f761aa68"
-  config.username       = "swapidy1"
-  config.password       = "postage1"
-  config.endpoint       = 'https://swsim.testing.stamps.com/swsim/swsimv24.asmx'.freeze
-  config.namespace      = 'http://stamps.com/xml/namespace/2012/05/swsim/swsimv24'.freeze
+  if Rails.env == 'production'
+    config.integration_id = ENV['STAMP_INTEGRATION_ID']
+    config.username       = ENV['STAMP_USERNAME']
+    config.password       = ENV['STAMP_PASSWORD']
+    config.endpoint       = ENV['STAMP_ENDPOINT'].freeze
+    config.namespace      = ENV['STAMP_NAMESPACE'].freeze
+  else
+    config.integration_id = "698b2f76-8f72-4359-b04f-1ca8f761aa68"
+    config.username       = "swapidy1"
+    config.password       = "postage1"
+    config.endpoint       = 'https://swsim.testing.stamps.com/swsim/swsimv24.asmx'.freeze
+    config.namespace      = 'http://stamps.com/xml/namespace/2012/05/swsim/swsimv24'.freeze
+  end
   config.log_messages   = true
   #config.return_address = { :full_name    => 'Swapidy Inc.',
   #                          :address1     => '2310 Rock Street Apt 38',
