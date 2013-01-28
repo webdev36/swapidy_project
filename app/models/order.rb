baa228f9a9e4bf70f64724cd1bb2f48fbfc12706
theswapdaddy <adam@swapidy.com>
2013-01-28 04:29:59 -0800

    Update app/models/order.rb

--- app/models/order.rb
+++ app/models/order.rb
@@ -70,7 +70,7 @@ class Order < ActiveRecord::Base
   
   def shipping_address_valid?
     unless ZipCode::ZIPCODES[self.shipping_zip_code]
-      errors.add(:shipping_zip_code, "is not supported. Please select another value!")
+      errors.add(:shipping_zip_code, "is not supported with Swapidy's services. Please try later.")
       return false
     end
     
