63e68bcdfbd1b26b5ecb1cf795addf99ca5eb6b6
Bui Thanh Hai - Scrum2B <hai.bth@gmail.com>
2013-02-02 15:04:46 +0700

    Test https for s3 images

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,8 @@ Swapidy::Application.configure do
                                   :bucket => ENV['S3_BUCKET_NAME'],
                                   :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
                                   :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-                                }
+                                },
+                                :s3_protocol => "https"
                               }
   config.action_mailer.default_url_options = { :host => 'swapidy.com' }
 
