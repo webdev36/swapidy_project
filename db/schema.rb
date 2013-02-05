caa9d50eb972c4728dde88a6c6f3285ee67963a8
Hung Kieu Viet <hungkv@ithanoi.com>
2013-02-05 11:38:14 +0700

    add schema

--- db/schema.rb
+++ db/schema.rb
@@ -24,8 +24,6 @@ ActiveRecord::Schema.define(:version => 20130202103654) do
     t.string  "title"
   end
 
-  add_index "category_attributes", ["category_id"], :name => "index_category_attributes_on_category_id"
-
   create_table "free_honeys", :force => true do |t|
     t.integer  "sender_id"
     t.integer  "receiver_id"
@@ -127,16 +125,11 @@ ActiveRecord::Schema.define(:version => 20130202103654) do
     t.string  "value"
   end
 
-  add_index "product_attributes", ["product_model_attribute_id"], :name => "index_product_attributes_on_product_model_attribute_id"
-
   create_table "product_model_attributes", :force => true do |t|
     t.integer "product_model_id"
     t.integer "category_attribute_id"
     t.string  "value"
   end
-
-  add_index "product_model_attributes", ["category_attribute_id"], :name => "index_product_model_attributes_on_category_attribute_id"
-  add_index "product_model_attributes", ["product_model_id"], :name => "index_product_model_attributes_on_product_model_id"
 
   create_table "product_models", :force => true do |t|
     t.string  "title"
@@ -144,8 +137,6 @@ ActiveRecord::Schema.define(:version => 20130202103654) do
     t.integer "category_id"
     t.decimal "weight_lb",   :default => 1.0
   end
-
-  add_index "product_models", ["category_id"], :name => "index_product_models_on_category_id"
 
   create_table "products", :force => true do |t|
     t.string  "title"
@@ -162,9 +153,6 @@ ActiveRecord::Schema.define(:version => 20130202103654) do
     t.integer "price_for_poor_buy"
   end
 
-  add_index "products", ["category_id"], :name => "index_products_on_category_id"
-  add_index "products", ["product_model_id"], :name => "index_products_on_product_model_id"
-
   create_table "rails_admin_histories", :force => true do |t|
     t.text     "message"
     t.string   "username"
@@ -175,8 +163,6 @@ ActiveRecord::Schema.define(:version => 20130202103654) do
     t.datetime "created_at",              :null => false
     t.datetime "updated_at",              :null => false
   end
-
-  add_index "rails_admin_histories", ["item", "table", "month", "year"], :name => "index_rails_admin_histories"
 
   create_table "redeem_codes", :force => true do |t|
     t.string   "code"
@@ -253,12 +239,5 @@ ActiveRecord::Schema.define(:version => 20130202103654) do
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
-
-  create_table "votes", :force => true do |t|
-    t.integer  "user_id"
-    t.string   "location_name"
-    t.datetime "created_at",    :null => false
-    t.datetime "updated_at",    :null => false
-  end
 
 end
