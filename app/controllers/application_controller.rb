eb24b2d72d345ce571e702af2cf8f8dad5d7def4
Bui Thanh Hai - Scrum2B <hai.bth@gmail.com>
2013-02-06 20:47:47 +0700

    Fix bugs in catch errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,7 +21,7 @@ class ApplicationController < ActionController::Base
   end
   
   unless Rails.application.config.consider_all_requests_local
-    rescue_from Exception, :with => :render_not_found
+    #rescue_from Exception, :with => :render_not_found
     rescue_from ActiveRecord::RecordNotFound, :with => :render_not_found
     rescue_from ActionController::RoutingError, :with => :render_not_found
     rescue_from ActionController::UnknownController, :with => :render_not_found
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -32,7 +32,7 @@ class SessionsController < Devise::SessionsController
 
       if user_signed_in?
         #check_to_display_guide
-        redirect_to :controller => :orders, :action => :new, :method => :post, :product_id => params[:product_id], :using_condition => params[:using_condition], :order_type => params[:order_type]
+        redirect_to :controller => :orders, :action => :new, :method => :post
       else
         @signin_failure = true
         @order = Order.new()
