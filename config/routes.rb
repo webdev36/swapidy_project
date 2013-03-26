e857e8de2640c71305d53eeacff29b37817fd808
gentle0219 <gentle0219@hotmail.com>
2013-03-27 06:54:56 +0800

    add the certified name option

--- config/routes.rb
+++ config/routes.rb
@@ -53,6 +53,8 @@ Swapidy::Application.routes.draw do
   match "/orders/complete" => "orders#complete", :method => :get
   match "/orders/change_email" => "orders#change_email", :method => :post
   match "/orders/change_paypal_email" => "orders#change_paypal_email", :method => :post
+  match "/orders/change_certified_name" => "orders#change_certified_name", :method => :post
+
   match "/orders/change_shipping_info" => "orders#change_shipping_info", :method => :post
   match "/orders/reload_payment_order_info" => "orders#reload_payment_order_info", :method => :post
 
