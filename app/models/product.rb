653c3e61638326a961e709477074c59e82b1edf3
gentle0219 <gentle0219@hotmail.com>
2013-03-30 14:45:39 +0800

    modify buy options filter

--- app/models/product.rb
+++ app/models/product.rb
@@ -117,16 +117,22 @@ class Product < ActiveRecord::Base
     prices.each_with_index do |price, index|
       if index == 0
         (result << "price_range_#{price}"; next) if price_for_sell && price_for_sell > 0 && price_for_sell <= price 
+        
+        (result << "price_range_#{price}"; next) if price_for_buy && price_for_buy > 0 && price_for_buy <= price 
 #        (result << "price_range_#{price}"; next) if price_for_good_sell && price_for_good_sell > 0 && price_for_good_sell <= price 
 #        (result << "price_range_#{price}"; next) if price_for_poor_sell && price_for_poor_sell > 0 && price_for_poor_sell <= price 
       elsif index < prices.size - 1
         (result << "price_range_#{price}"; next) if price_for_sell && price_for_sell >= prices[index - 1] && price_for_sell <= price 
         (result << "price_range_#{price}"; next) if price_for_good_sell && price_for_good_sell >= prices[index - 1] && price_for_good_sell <= price 
-        (result << "price_range_#{price}"; next) if price_for_poor_sell && price_for_poor_sell >= prices[index - 1] && price_for_poor_sell <= price
+        (result << "price_range_#{price}"; next) if price_for_poor_sell && price_for_poor_sell >= prices[index - 1] && price_for_poor_sell <= price        
+        
+        (result << "price_range_#{price}"; next) if price_for_buy && price_for_buy >= prices[index - 1] && price_for_buy <= price 
       else
         (result << "price_range_#{price}"; next) if price_for_sell && price_for_sell >= price 
         (result << "price_range_#{price}"; next) if price_for_good_sell && price_for_good_sell >= price 
         (result << "price_range_#{price}"; next) if price_for_poor_sell && price_for_poor_sell >= price
+        
+        (result << "price_range_#{price}"; next) if price_for_buy && price_for_buy >= price 
       end
     end
     
