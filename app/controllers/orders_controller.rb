745e0a1880436a5c918cb8c4bce78958cc4c9808
gentle0219 <gentle0219@hotmail.com>
2013-03-31 11:16:47 +0800

    modify

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -68,8 +68,8 @@ class OrdersController < ApplicationController
 
             if @order.save
                 @order.create_stamp_to_deliver(session[:shop_type])
-                OrderNotifier.delay.start_processing(@order, session[:shop_type]).deliver
-                OrderNotifier.delay.start_processing_for_admin(@order, session[:shop_type]).deliver
+                OrderNotifier.delay.start_processing(@order, session[:shop_type])
+                OrderNotifier.delay.start_processing_for_admin(@order, session[:shop_type])
                 ShoppingCart.delay.clear_cart_products 
              end
           end          
@@ -92,8 +92,8 @@ class OrdersController < ApplicationController
             if @order.save
               @order.do_payment
               @order.create_stamp_to_deliver(session[:shop_type])
-              OrderNotifier.delay.start_processing(@order, session[:shop_type]).deliver
-              OrderNotifier.delay.start_processing_for_admin(@order,session[:shop_type]).deliver
+              OrderNotifier.delay.start_processing(@order, session[:shop_type])
+              OrderNotifier.delay.start_processing_for_admin(@order,session[:shop_type])
               ShoppingCart.delay.clear_cart_products
             end
           end
@@ -122,8 +122,8 @@ class OrdersController < ApplicationController
             if @order.save
               @order.do_payment
               @order.create_stamp_to_deliver(session[:shop_type])
-              OrderNotifier.delay.start_processing(@order, session[:shop_type]).deliver
-              OrderNotifier.delay.start_processing_for_admin(@order,session[:shop_type]).deliver
+              OrderNotifier.delay.start_processing(@order, session[:shop_type])
+              OrderNotifier.delay.start_processing_for_admin(@order,session[:shop_type])
               ShoppingCart.delay.clear_cart_products 
             end
           end
